013df20d472eb1b3bf151b94902d8739b51a86ed
Jasmine Liang <jasmine.liang@walmartlabs.com>
2019-07-24 14:32:12 -0700

    Uncomment x-pack from pm build

--- packages/kbn-pm/dist/index.js
+++ packages/kbn-pm/dist/index.js
@@ -17883,8 +17883,8 @@ function getProjectPaths(rootPath, options) {
     // correct and the expect behavior.
     projectPaths.push((0, _path.resolve)(rootPath, 'test/plugin_functional/plugins/*'));
     if (!ossOnly) {
-        // projectPaths.push(resolve(rootPath, 'x-pack'));
-        // projectPaths.push(resolve(rootPath, 'x-pack/plugins/*'));
+        projectPaths.push((0, _path.resolve)(rootPath, 'x-pack'));
+        projectPaths.push((0, _path.resolve)(rootPath, 'x-pack/plugins/*'));
     }
     if (!skipKibanaExtra) {
         projectPaths.push((0, _path.resolve)(rootPath, '../kibana-extra/*'));
